d3c890ba9abbb11804cfd3fdd6d26a93052f7e7c
treyson tsen <78147374+p0nyo@users.noreply.github.com>
2025-05-24 10:39:30 +1200

    add: recursive + test cases

--- codekata-02-karate-chop.py
+++ codekata-02-karate-chop.py
@@ -1,3 +1,5 @@
+import pytest
+
 def binary_search_iterative(num_list, num):
     low = 0
     high = len(num_list) - 1
@@ -12,11 +14,71 @@ def binary_search_iterative(num_list, num):
 
         else:
             low = mid + 1
-    
     return -1
 
 
-num_list = [2, 3, 4, 10, 40]
-num = 10
+def binary_search_recursive(num_list, low, high, num):
+    while low <= high:
+        mid = low + (high-low) // 2
 
-print(binary_search_iterative(num_list, num))
+        if num_list[mid] == num:
+            return mid
+        
+        elif num_list[mid] > num:
+            return binary_search_recursive(num_list, low, mid-1, num)
+
+        else:
+            return binary_search_recursive(num_list, mid+1, high, num)
+    return -1
+
+
+
+
+class TestBinarySearch:
+    def test_binary_search_iterative(self):
+        num_list = [2, 3, 4, 10, 40]
+        num = 10
+
+        assert binary_search_iterative(num_list, num) == 3
+
+    def test_binary_search_iterative_first(self):
+        num_list = [2, 3, 4, 10, 40]
+        num = 2
+
+        assert binary_search_iterative(num_list, num) == 0
+
+    def test_binary_search_iterative_last(self):
+        num_list = [2, 3, 4, 10, 40]
+        num = 40
+
+        assert binary_search_iterative(num_list, num) == 4
+
+    def test_binary_search_iterative_empty_list(self):
+        num_list = []
+        num = 10
+
+        assert binary_search_iterative(num_list, num) == -1
+
+    def test_binary_search_recursive(self):
+        num_list = [2, 3, 4, 10, 40]
+        num = 10
+
+        assert binary_search_recursive(num_list, 0, len(num_list)-1, num) == 3
+
+    def test_binary_search_recursive_first(self):
+        num_list = [2, 3, 4, 10, 40]
+        num = 2
+
+        assert binary_search_recursive(num_list, 0, len(num_list)-1, num) == 0
+
+    def test_binary_search_recursive_last(self):
+        num_list = [2, 3, 4, 10, 40]
+        num = 40
+
+        assert binary_search_recursive(num_list, 0, len(num_list)-1, num) == 4
+
+    def test_binary_search_recursive_empty_list(self):
+        num_list = []
+        num = 10
+
+        assert binary_search_recursive(num_list, 0, len(num_list)-1, num) == -1
